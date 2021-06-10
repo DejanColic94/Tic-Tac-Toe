5ec3488d7dad4fe09c163e509f8f12ba58e25608
Dejan Colic <dejan.colic99@gmail.com>
2021-06-11 00:55:07 +0200

    implemented win condition

--- src/index.js
+++ src/index.js
@@ -23,6 +23,10 @@ import './index.css';
 
     handleClick(i) {
       const squares = this.state.squares.slice();
+      // abort the game if there is a winner
+      if(calculateWinner(squares) || squares[i]) {
+        return;
+      }
       squares[i] = this.state.xIsNext ? "X" : "O";
       this.setState({ 
         squares : squares,
@@ -37,7 +41,13 @@ import './index.css';
     }
   
     render() {
-      const status = 'Next player: '+(this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if(winner) {
+        status = 'Winner is : ' + winner;
+      }else {
+        status = "Next player : "+ (this.state.xIsNext ? "X" : "O");
+      }
   
       return (
         <div>
@@ -84,3 +94,25 @@ import './index.css';
     <Game />,
     document.getElementById('root')
   );
+
+  // checks for 3 of the same characters ( X or O) in a row row-wise
+  // column-wise and on the diagonals and returns X or O
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2], // rows
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6], // columns
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8], // diagonals
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
